a472b415732d3fa9ae738033c316d0ad8a23c0f5
Евгений Бузуев <ezjell@yandex.ru>
2023-01-30 15:07:19 +0300

    added script to find out mouse position

--- main.py
+++ main.py
@@ -4,15 +4,46 @@ from abc import ABC, abstractmethod
 
 if __name__ == '__main__':
 
-    def click_on_wb_app(x: float, y: float):
-        pyautogui.click(x, y)
+    def click_on_wb_app(wildberies_icon):
+        """Находим приложение вб по картинке"""
+        pyautogui.click(wildberies_icon)
+        # pyautogui.click(x, y)
 
-    def click_on_wb_search_tab(x: float, y: float):
-        pyautogui.click(x, y)
+    def click_on_wb_search_tab(wb_search_tab):
+        """"Находим строку поиска"""
+        pyautogui.click(wb_search_tab)
+        # pyautogui.click(x, y)
     
     def go_to_searching_product_list(text: str, interval: float):
+        """"Забиваем ключевое слово и осуществляем поиск"""
         pyautogui.typewrite(text, interval)
         pyautogui.press('enter')
     
-    def make_a_print_screen():
-        
+    def locate_product(picture):
+        """Находим необходимый продукт по картинке, либо скролим дальше"""
+        while True:
+            try:
+                pyautogui.click(picture)
+                break
+            except:
+                pyautogui.scroll(2)
+    
+    def add_to_bucket(bucket_picture):
+        pyautogui.click(bucket_picture)
+
+    def my_backet_tab_click(my_bucket_pic):
+        pyautogui.click(my_bucket_pic)
+    
+    def get_in_my_acc(my_acc_tab):
+        pyautogui.click(my_acc_tab)
+    
+    def log_in(log_in_tab):
+        pyautogui.click(log_in_tab)
+    
+    def log_in_with_cellphone(cell_num_pic, cell_number):
+        pyautogui.click(cell_num_pic)
+        pyautogui.typewrite(cell_number)
+        pyautogui.press('enter')
+
+    def to_buy_order(buy_order_pic):
+        pyautogui.click(buy_order_pic)
---
+++ positions.py
@@ -0,0 +1,21 @@
+import os
+import random
+import time
+import pyautogui
+
+
+pyautogui.PAUSE = 0.2
+pyautogui.FAILSAFE = True
+
+print('Для выхода используйте <Ctrl+C')
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        position_str = f'X: {str(x).rjust(4)} Y: {str(y).rjust(4)}'
+        print(position_str, end='')
+        print('\b' * len(position_str), end='', flush=True)
+
+except:
+    os.system('clear')
+    print('программа завершена')
