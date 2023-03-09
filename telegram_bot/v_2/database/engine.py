361657651eb1722a84e21f62fe764a2003834620
Евгений Бузуев <ezjell@yandex.ru>
2023-03-09 23:28:03 +0300

    setting up postgre database

---
+++ telegram_bot/v_2/database/engine.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+def create_async_engine(url: URL|str) -> AsyncEngine:
+    return create_engine(url=url, echo=True, encoding='utf-8', pool_pre_ping=True)
+
+
+def proceed_schemas(session: AsyncSession, metadata) -> None:
+    with session.begin():
+        session.run_sync(metadata.create_all)
+
+def get_session_maker(engine: AsyncEngine) -> sessionmaker:
+    return sessionmaker(engine, class_=AsyncEngine)
+ 
