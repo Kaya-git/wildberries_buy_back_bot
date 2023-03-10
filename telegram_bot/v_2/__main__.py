9559d4a98d4140e556a2f0f98300abecbc154baa
Евгений Бузуев <ezjell@yandex.ru>
2023-03-10 09:38:24 +0300

    wrong postgres_url param

--- telegram_bot/v_2/__main__.py
+++ telegram_bot/v_2/__main__.py
@@ -5,7 +5,8 @@ from commands.bot_commands import bot_commands
 import logging
 import asyncio
 from aiogram.types import BotCommand
-
+from sqlalchemy.engine import URL
+from database import BaseModel, create_async_engine, get_session_maker, proceed_schemas
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
@@ -22,6 +23,19 @@ async def main():
     await bot.set_my_commands(commands=commands_for_bot)
 
     register_user_commands(dp)
+
+    postgres_url = URL.create(
+        "postgresql+asyncpg",
+        username="postgres",
+        host="localhost",
+        password="WW9JUQhP",
+        database="BuyBackBase",
+        port="5432"
+    )
+
+    async_engine = create_async_engine(postgres_url)
+    session_maker = get_session_maker(async_engine)
+    await proceed_schemas(async_engine, BaseModel.metadata)
 
     await dp.start_polling(bot)
 
--- telegram_bot/v_2/database/engine.py
+++ telegram_bot/v_2/database/engine.py
@@ -9,9 +9,9 @@ def create_async_engine(url: URL|str) -> AsyncEngine:
     return create_engine(url=url, echo=True, encoding='utf-8', pool_pre_ping=True)
 
 
-def proceed_schemas(session: AsyncSession, metadata) -> None:
-    with session.begin():
-        session.run_sync(metadata.create_all)
+async def proceed_schemas(engine: AsyncEngine, metadata) -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
 
 def get_session_maker(engine: AsyncEngine) -> sessionmaker:
     return sessionmaker(engine, class_=AsyncEngine)
